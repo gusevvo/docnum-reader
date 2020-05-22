a12d4951611954e490699cd42b75df79674cb799
Valeriy Gusev <valeriygusev@Valeriys-MacBook-Pro.local>
2020-05-22 23:59:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/docnumreader/FirebaseTextRecognitionAnalyzer.java
@@ -0,0 +1,138 @@
+package com.example.docnumreader;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.Image;
+import android.util.Log;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.util.List;
+
+public class FirebaseTextRecognitionAnalyzer implements ImageAnalysis.Analyzer {
+
+    private static final String TAG = "MY-FIRE";
+
+    public interface ScannerAnalyzerListener {
+        void call(String s);
+    }
+
+    private ScannerAnalyzerListener listener;
+    private ImageView cropRectImageView;
+
+    public FirebaseTextRecognitionAnalyzer(ScannerAnalyzerListener listener, ImageView cropRectImageView) {
+        this.listener = listener;
+        this.cropRectImageView = cropRectImageView;
+    }
+
+    @Override
+    public void analyze(@NonNull ImageProxy imageProxy) {
+        int rotation = degreesToFirebaseRotation(imageProxy.getImageInfo().getRotationDegrees());
+
+        Image image = imageProxy.getImage();
+//        Bitmap bmp = getBitmap(image);
+//
+//        int left = cropRectImageView.getLeft();
+//        int top = cropRectImageView.getTop();
+//        int width = cropRectImageView.getWidth();
+//        int height = cropRectImageView.getHeight();
+//
+//        Bitmap croped = null;
+//        try {
+//            croped = Bitmap.createBitmap(
+//                    bmp,
+//                    cropRectImageView.getLeft(), //x
+//                    cropRectImageView.getTop(),//y
+//                    cropRectImageView.getWidth(), //width
+//                    cropRectImageView.getHeight()  //height
+//            );
+//        } catch (Exception ex) {
+//            Log.e(TAG, "Ошибка распознование текста. Ошибк createBitmap", ex);
+//
+//        }
+
+
+        FirebaseVisionImage firebaseVisionImage;
+        FirebaseVisionTextRecognizer detector;
+
+        try {
+            firebaseVisionImage = FirebaseVisionImage.fromMediaImage(image, rotation);
+            //firebaseVisionImage = FirebaseVisionImage.fromBitmap(croped);
+            detector = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+        } catch (Exception ex) {
+            Log.e(TAG, "Ошибка распознование текста. Ошибка инициализации детектора", ex);
+            return;
+        }
+
+        Task<FirebaseVisionText> result =
+                detector.processImage(firebaseVisionImage)
+                        .addOnSuccessListener(firebaseVisionText -> {
+                            String text = firebaseVisionText.getText();
+                            List<FirebaseVisionText.TextBlock> block = firebaseVisionText.getTextBlocks();
+
+                            listener.call(firebaseVisionText.getText());
+
+                        })
+                        .addOnFailureListener(
+                                ex -> {
+                                    Log.e(TAG, "Ошибка распознование текста. Ошибка обработки изображения", ex);
+                                });
+
+        image.close();
+    }
+
+    private int degreesToFirebaseRotation(int degrees) {
+        switch (degrees) {
+            case 0:
+                return FirebaseVisionImageMetadata.ROTATION_0;
+            case 90:
+                return FirebaseVisionImageMetadata.ROTATION_90;
+            case 180:
+                return FirebaseVisionImageMetadata.ROTATION_180;
+            case 270:
+                return FirebaseVisionImageMetadata.ROTATION_270;
+            default:
+                throw new IllegalArgumentException(
+                        "Rotation must be 0, 90, 180, or 270.");
+        }
+    }
+
+    private Bitmap getBitmap(Image image) {
+        Image.Plane[] planes = image.getPlanes();
+        ByteBuffer yBuffer = planes[0].getBuffer();
+        ByteBuffer uBuffer = planes[1].getBuffer();
+        ByteBuffer vBuffer = planes[2].getBuffer();
+
+        int ySize = yBuffer.remaining();
+        int uSize = uBuffer.remaining();
+        int vSize = vBuffer.remaining();
+
+        byte[] nv21 = new byte[ySize + uSize + vSize];
+        //U and V are swapped
+        yBuffer.get(nv21, 0, ySize);
+        vBuffer.get(nv21, ySize, vSize);
+        uBuffer.get(nv21, ySize + vSize, uSize);
+
+        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 75, out);
+
+        byte[] imageBytes = out.toByteArray();
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+    }
+}
---
+++ app/src/main/java/com/example/docnumreader/MainActivity.java
@@ -0,0 +1,157 @@
+package com.example.docnumreader;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MY-FIRE";
+
+    private TextView textView;
+    private ImageView cropRectImageView;
+    private PreviewView previewView;
+
+    private ImageAnalysis imageAnalysis;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    /**
+     * Blocking camera operations are performed using this executor
+     */
+    private ExecutorService cameraExecutor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraExecutor != null) {
+            cameraExecutor.shutdown();
+        }
+    }
+
+    private void init() {
+        textView = findViewById(R.id.text_view);
+        cropRectImageView = findViewById(R.id.image_view_crop_rect);
+        previewView = findViewById(R.id.preview_view);
+
+        // Initialize background executor for ImageAnalysis
+        cameraExecutor = Executors.newSingleThreadExecutor();
+
+        if (isCameraPermissionGranted()) {
+            bindCameraAfterViewFinderInflated();
+        } else {
+            requestCameraPermission();
+        }
+    }
+
+    private static final int CAMERA_PERMISSION_REQUEST = 100;
+
+    private boolean isCameraPermissionGranted() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestCameraPermission() {
+        final String[] cameraPermission = new String[]{Manifest.permission.CAMERA};
+        ActivityCompat.requestPermissions(this, cameraPermission, CAMERA_PERMISSION_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode != CAMERA_PERMISSION_REQUEST) {
+            return;
+        }
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) { //if (isCameraPermissionGranted()) {
+            bindCameraAfterViewFinderInflated();
+        } else {
+            String msg = "Ошибка распознование текста. Отказано в доступе к камере";
+
+        }
+    }
+
+    private void bindCameraAfterViewFinderInflated() {
+        previewView.post(this::bindCameraUseCases);
+    }
+
+
+    private void bindCameraUseCases() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(this::cameraProviderFutureListener, ContextCompat.getMainExecutor(this));
+    }
+
+    private void cameraProviderFutureListener() {
+        ProcessCameraProvider cameraProvider;
+        try {
+            cameraProvider = cameraProviderFuture.get();
+        } catch (Exception ex) {
+            showErrorMessage("Ошибка распознование текста. Ошибка инициализации камеры (Camera provider)", ex);
+            return;
+        }
+
+        int rotation = previewView.getDisplay().getRotation();
+        Preview preview = new Preview.Builder()
+                .setTargetRotation(rotation)
+                .build();
+
+        imageAnalysis = new ImageAnalysis.Builder()
+                .setTargetRotation(rotation)
+                .setTargetResolution(new Size(720, 1280))
+                .build();
+        imageAnalysis.setAnalyzer(cameraExecutor, new FirebaseTextRecognitionAnalyzer(this::onSuccess, cropRectImageView));
+
+        cameraProvider.unbindAll();
+        try {
+            Camera camera = cameraProvider.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageAnalysis);
+            preview.setSurfaceProvider(previewView.createSurfaceProvider(camera.getCameraInfo()));
+        } catch (Exception ex) {
+            showErrorMessage("Ошибка распознование текста. Ошибка инициализации камеры (Use cases)", ex);
+        }
+    }
+
+    private void onSuccess(String content) {
+        try {
+            runOnUiThread(() -> {
+                Log.d(TAG, content);
+                textView.setText(content);
+            });
+        } catch (Exception ex) {
+            showErrorMessage("Ошибка распознование текста. Не удалось установить распознанное значение", ex);
+        }
+    }
+
+    private void showErrorMessage(String message, Throwable ex) {
+        Log.e(TAG, message, ex);
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+}
